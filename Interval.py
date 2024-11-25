10209538c6e9305c5aa5d35adc8ceacb2320990a
mathieu stamm <mathieu.stamm@gmail.com>
2024-11-25 15:29:16 +0100

    update interval

--- Interval.py
+++ Interval.py
@@ -1,6 +1,6 @@
 x = float(input("Entrez un nombre réel : "))
 
 if (2 <= x < 3) or (0 < x <= 1) or (-10 <= x < -2):
-    print("x appartient à I")
+    print(f"{int(x)} appartient à I")
 else:
-    print("x n'appartient pas à I")
+    print(f"{int(x)} n'appartient pas à I")
